88b2af2e7dd1daf1913488fdc9f30b89c193477e
Mariia Glatsyna <mariia.galitsyna@gmail.com>
2021-06-30 08:15:20 +0200

    lifting the state and using lifecycle componentDidMount method

--- src/App.js
+++ src/App.js
@@ -7,6 +7,55 @@ import Gallery from "./components/gallery";
 import About from "./components/about";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handlePage = this.handlePage.bind(this);
+    this.state = {};
+  }
+
+  // gettingNews = async (e) => {
+  //   e.preventDefault();
+  //   const api_key = "FssgzRxRGe1elBmwvuQYcqfZPBBSr4dX3HQryjFX";
+
+  //   const today = new Date();
+  //   let newsNumber = 6;
+  //   const previousDay = ((d) => new Date(d.setDate(d.getDate() - newsNumber)))(
+  //     new Date()
+  //   );
+  //   const date = today.toJSON().slice(0, 10);
+  //   const previousDate = previousDay.toJSON().slice(0, 10);
+
+  //   let daysCollection = [];
+  //   let infoCollection = [];
+
+  //   const api_url =
+  //     await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&start_date=${previousDate}&end_date=${date}
+  //   `);
+
+  //   const data = await api_url.json();
+
+  //   for (let value of data) {
+  //     daysCollection.push(value.date);
+  //     infoCollection.push(value.explanation);
+  //   }
+
+  //   this.setState({
+  //     date: daysCollection.reverse(),
+  //     info: infoCollection,
+  //   });
+  //   console.log(this.state.date);
+  // };
+
+  handlePage(url) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) =>
+        this.setState({
+          news: result.map((res) => <div>{res.explanation}</div>),
+        })
+      );
+  }
+
   render() {
     return (
       <BrowserRouter>
@@ -16,7 +65,7 @@ class App extends React.Component {
         <div>
           <Switch>
             <Route exact path="/">
-              <Home />
+              <Home handleNews={this.handlePage} news={this.state.news} />
             </Route>
             <Route path="/about">
               <About />
