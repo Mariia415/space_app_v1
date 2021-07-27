a39effeb8abbd3f4675978364c91360bda22c4eb
Mariia Glatsyna <mariia.galitsyna@gmail.com>
2021-07-27 11:45:47 +0200

    updating

--- src/components/gallery/galleryCard.js
+++
@@ -1,8 +0,0 @@
-export const GalleryCard = ({ url, title }) => {
-  return (
-    <div className="gallery-card">
-      <img className="gallery-img" src={url} alt="Wait" />
-      <p className="gallery-title">{title}</p>
-    </div>
-  );
-};
--- src/components/home/newsCard.js
+++
@@ -1,8 +0,0 @@
-export const NewsCard = ({ day, news }) => {
-  return (
-    <div className="news-card">
-      <div className="news-date">{day}</div>
-      <div className="news-text">{news}</div>
-    </div>
-  );
-};
