fcda539615830030ae68124b92260dd3c2bf1271
Mariia Glatsyna <mariia.galitsyna@gmail.com>
2021-07-27 11:42:05 +0200

    updating

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import "./App.css";
 import App from "./App";
+import store from "./store";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
