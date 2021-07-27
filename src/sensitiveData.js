968be354dad39a012c7686d59c1e64df905f4a10
Mariia Glatsyna <mariia.galitsyna@gmail.com>
2021-07-27 11:05:53 +0200

    resolving conflict

--- src/sensitiveData.js
+++ src/sensitiveData.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-const api_key = "FssgzRxRGe1elBmwvuQYcqfZPBBSr4dX3HQryjFX";
-export const newsNumber = 5;
-export const request_url = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&count=${newsNumber}`;
-=======
 export const api_key = `FssgzRxRGe1elBmwvuQYcqfZPBBSr4dX3HQryjFX`;
 const period = 5;
 const date = new Date();
@@ -14,4 +9,3 @@ const prevDate = new Date(
 
 export const startDate = prevDate.toJSON().slice(0, 10);
 export const endDate = date.toJSON().slice(0, 10);
->>>>>>> SAV-001-redux-thunk_implementation
