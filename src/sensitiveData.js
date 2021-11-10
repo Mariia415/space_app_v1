75ce85e19b973e0019517f5e046afe8707d0858a
Maria <mariia.galitsyna@gmail.com>
2021-11-10 21:53:21 +0100

    improve authorization check

--- src/sensitiveData.js
+++ src/sensitiveData.js
@@ -7,8 +7,10 @@ const prevDate = new Date(
   date.getDate() - period
 );
 
-const accessCodes = ['1234', '2345', '3456', '78910'];
+const accessCodes = ['1234', '2345', '3456', '78910', '23476', '2k4567', 'ff7869', '23gs'];
 
 export const startDate = prevDate.toJSON().slice(0, 10);
 export const endDate = date.toJSON().slice(0, 10);
-export const guestAccessCodes = accessCodes.slice();
+export const guestAccessCodes = accessCodes.filter(item => {
+  return item.length === 4 && !isNaN(Number(item));
+});
